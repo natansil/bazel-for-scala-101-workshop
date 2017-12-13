7fed8c5449bbb4d9f94a6d84d49c68879d6c752c
natans <natans@wix.com>
2017-12-14 01:17:23 +0200

    fix version disparity

--- 03-missing-external-dependency/src/main/scala/com/example/Example.scala
+++ 03-missing-external-dependency/src/main/scala/com/example/Example.scala
@@ -1,6 +1,6 @@
 package com.example
 
 object Example {
-	// utilizes com.google.guava:guava:21.0
+	// utilizes com.google.guava:guava:18.0
 	com.google.common.base.Strings.commonPrefix("abc", "abcd")
 }
